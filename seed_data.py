34f2620e69be219486846901c7f80c413b0275d6
Shoofle Munroe <xsigma@gmail.com>
2015-10-20 14:04:55 -0400

    Generate matching and potentially-nonmatching parens

--- seed_data.py
+++ seed_data.py
@@ -16,20 +16,38 @@ def check_parens(test_string):
 	else:
 		return not stack
 
-print(check_parens("{{[[]}]}"))
-print(check_parens("(())()[][{}]"))
-
-def piece(depth):
+def good_piece(depth):
 	if depth == 0:
 		return ""
-	elif random.choice([True, False]):
+	idx = random.randint(0, 1)
+	if idx == 0:
 		opening = random.choice(bracket_open)
 		closing = bracket_close[bracket_open.find(opening)]
-		return "{}{}{}".format(opening, piece(depth - 1), closing)
-	else:
-		return "{}{}".format(piece(depth - 1), piece(depth - 1))
+		return "{}{}{}".format(opening, good_piece(depth - 1), closing)
+	elif idx == 1:
+		return "{}{}".format(good_piece(depth - 1), good_piece(depth - 1))
+
+def bad_piece(depth):
+	result = good_piece(depth)
+
+	if len(result) == 0:
+		return result
+
+	for i in range(5):
+		index = random.randint(0, len(result) - 1)
+		insert = random.choice(random.choice([bracket_close, bracket_open]))
+		return result[:index-1] + insert + result[index:]
+
+print("The good!")
 
 for i in range(10):
-	this_string = piece(i)
-	print(this_string, check_parens(this_string))
+	this_string = good_piece(i)
+	print(check_parens(this_string), this_string)
+
+print()
+print("The bad!")
+
+for i in range(10):
+	this_string = bad_piece(i)
+	print(check_parens(this_string), this_string)
 
